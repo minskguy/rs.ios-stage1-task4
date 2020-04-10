f090e7987a8bfe995ebb9a7eb797a1dcd7dbcb3d
Mark <macvik17@mail.ru>
2020-04-10 18:53:37 +0300

    feat: add solution to task2

--- RSSchool_T4/RSSchool_T4/2/SquareDecomposer.m
+++ RSSchool_T4/RSSchool_T4/2/SquareDecomposer.m
@@ -2,7 +2,38 @@
 
 @implementation SquareDecomposer
 - (NSArray <NSNumber*>*)decomposeNumber:(NSNumber*)number {
-    // good luck
-    return @[@0];
+    NSMutableArray *result = [[NSMutableArray alloc] init];
+    if (recursion([NSNumber numberWithLongLong:[number longLongValue]*[number longLongValue]],
+              number,
+              result,
+              number) == TRUE) {
+        result = [[[result reverseObjectEnumerator] allObjects] mutableCopy];
+        return result;
+    };
+    return nil;
+}
+
+bool recursion(NSNumber* value,
+               NSNumber* previous,
+               NSMutableArray<NSNumber*>* result,
+               NSNumber* start) {
+    if([previous longLongValue] == 1 && [value longLongValue] != 0) return FALSE;
+    if([value longLongValue] == 0) return TRUE;
+    long long maximumSquare = sqrt([value longLongValue]);
+    if(maximumSquare*maximumSquare == [value longLongValue]
+       && [previous longLongValue] == [start longLongValue]) maximumSquare--;
+    for(long long i = maximumSquare; i >= 1; i--)
+    {
+        if(i < [previous longLongValue])
+        {
+            [result addObject:[NSNumber numberWithLongLong:i]];
+            if(recursion([NSNumber numberWithLongLong:[value longLongValue] - i * i],
+                         [NSNumber numberWithLongLong:i],
+                         result,
+                         start) == TRUE) return TRUE;
+            [result removeLastObject];
+        }
+    }
+    return FALSE;
 }
 @end
